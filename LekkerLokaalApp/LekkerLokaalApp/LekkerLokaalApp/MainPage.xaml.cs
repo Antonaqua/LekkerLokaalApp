f3d0d085a7396090f87ed751dcfe52548eb8e635
anton engels <anton.engels.w0190@student.hogent.be>
2018-05-22 14:29:54 +0200

    LekkerLokaalApp

---
+++ LekkerLokaalApp/LekkerLokaalApp/LekkerLokaalApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
+
+namespace LekkerLokaalApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenScanner(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        public async void Scanner()
+        {
+
+            var ScannerPage = new ZXingScannerPage();
+
+            ScannerPage.OnScanResult += (result) => {
+
+                ScannerPage.IsScanning = false;
+
+                if(result.Equals("22"))
+                {
+                    Device.BeginInvokeOnMainThread(() => {
+                        Navigation.PopAsync();
+                        DisplayAlert("", "Bon succesvol gescand!", "OK");
+                    });
+                } else
+                {
+                    Device.BeginInvokeOnMainThread(() => {
+                        Navigation.PopAsync();
+                        DisplayAlert("", "Er is iets misgelopen.", "OK");
+                    });
+                }
+                
+            };
+
+
+            await Navigation.PushAsync(ScannerPage);
+
+        }
+    }
+}
